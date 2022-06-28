93e3937c253c5c35797c6e6e538f4afbde1577ac
Wilfred Morgan <wmorgan@3cloudsolutions.com>
2022-06-28 16:46:55 -0400

    Implement generic repository

--- WiredBrainCoffee.StorageApp/Program.cs
+++ WiredBrainCoffee.StorageApp/Program.cs
@@ -1,11 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using WiredBrainCoffee.StorageApp.Entities;
 using WiredBrainCoffee.StorageApp.Repositories;
 
-EmployeeRepository employeeRepository = new();
-employeeRepository.Add(new WiredBrainCoffee.StorageApp.Entities.Employee { FirstName = "Julia" });
-employeeRepository.Add(new WiredBrainCoffee.StorageApp.Entities.Employee { FirstName = "Anna" });
-employeeRepository.Add(new WiredBrainCoffee.StorageApp.Entities.Employee { FirstName = "Thomas" });
+GenericRepository<Employee> employeeRepository = new();
+employeeRepository.Add(new Employee { FirstName = "Julia" });
+employeeRepository.Add(new Employee { FirstName = "Anna" });
+employeeRepository.Add(new Employee { FirstName = "Thomas" });
 employeeRepository.Save();
+
+GenericRepository<Organization> organizationRepository = new();
+organizationRepository.Add(new Organization { Name = "Pluralsight" });
+organizationRepository.Add(new Organization { Name = "Globomatics" });
+organizationRepository.Save();
 
 Console.ReadLine();
 
---
+++ WiredBrainCoffee.StorageApp/Repositories/GenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using WiredBrainCoffee.StorageApp.Entities;
+
+namespace WiredBrainCoffee.StorageApp.Repositories
+{
+    public class GenericRepository<T>
+    {
+        private readonly List<T> _items = new();
+
+        public void Add(T item) => _items.Add(item);
+
+        public void Save()
+        {
+            foreach (T item in _items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
