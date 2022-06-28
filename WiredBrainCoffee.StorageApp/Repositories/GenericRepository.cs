be1bc4ee3243f46ca7bd058ec08edaa816fd71f4
Wilfred Morgan <wmorgan@3cloudsolutions.com>
2022-06-28 17:48:09 -0400

    Implement new constraint

--- WiredBrainCoffee.StorageApp/Repositories/GenericRepository.cs
+++ WiredBrainCoffee.StorageApp/Repositories/GenericRepository.cs
@@ -2,11 +2,13 @@
 
 namespace WiredBrainCoffee.StorageApp.Repositories
 {
-    public class GenericRepository<T> where T : class, IEntity
+    public class GenericRepository<T> where T : class, IEntity, new() //must be last constraint declared
     {
         private readonly List<T> _items = new();
 
         public T? GetById(int id) => _items.Single(item => item.Id == id);
+
+        public T CreateItem() => new T();
 
         public void Add(T item)
         {
