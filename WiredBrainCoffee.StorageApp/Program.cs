95a9ba2bef9cec784908d892d92962b9c3de4aea
Wilfred Morgan <wmorgan@3cloudsolutions.com>
2022-06-28 16:35:55 -0400

    Implement EmployeeRepository

--- WiredBrainCoffee.StorageApp/Program.cs
+++ WiredBrainCoffee.StorageApp/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using WiredBrainCoffee.StorageApp.Repositories;
+
+EmployeeRepository employeeRepository = new();
+employeeRepository.Add(new WiredBrainCoffee.StorageApp.Entities.Employee { FirstName = "Julia" });
+employeeRepository.Add(new WiredBrainCoffee.StorageApp.Entities.Employee { FirstName = "Anna" });
+employeeRepository.Add(new WiredBrainCoffee.StorageApp.Entities.Employee { FirstName = "Thomas" });
+employeeRepository.Save();
+
+Console.ReadLine();
+
---
+++ WiredBrainCoffee.StorageApp/Repositories/EmployeeRepository.cs
@@ -0,0 +1,19 @@
+﻿using WiredBrainCoffee.StorageApp.Entities;
+
+namespace WiredBrainCoffee.StorageApp.Repositories
+{
+    public class EmployeeRepository
+    {
+        private readonly List<Employee> _employees = new();
+
+        public void Add(Employee employee) => _employees.Add(employee);
+
+        public void Save()
+        {
+            foreach (Employee employee in _employees)
+            {
+                Console.WriteLine(employee);
+            }
+        }
+    }
+}
