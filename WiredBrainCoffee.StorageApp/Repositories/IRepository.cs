50f10c13e22265c871a405dcd904e3123cfbbbc1
Wilfred Morgan <wmorgan@3cloudsolutions.com>
2022-06-29 16:09:31 -0400

    Add generic interface

---
+++ WiredBrainCoffee.StorageApp/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using WiredBrainCoffee.StorageApp.Entities;
+
+namespace WiredBrainCoffee.StorageApp.Repositories
+{
+    public interface IRepository<T> where T : IEntity
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        void Add(T item);
+        void Remove(T item);
+        void Save();
+    }
+}
